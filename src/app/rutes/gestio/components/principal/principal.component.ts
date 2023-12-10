f012c070831c1b13fa5ff4bdc6a56c5fb58ecd24
aramm <aram.mateos@institutvidreres.cat>
2023-12-10 15:42:10 +0100

    ARNAU AIXO FUNCIONA

--- src/app/rutes/gestio/components/principal/principal.component.ts
+++ src/app/rutes/gestio/components/principal/principal.component.ts
@@ -14,13 +14,14 @@ export class PrincipalComponent  implements OnInit {
 
 
   constructor(private http: HttpClient) {
+
   }
 
   ngOnInit() {
   }
 
   conexio() {
-    this.socket = io('http://localhost:3080', {transports: ['websocket']});
+    this.socket = io('http://192.168.1.134:7777', {transports: ['websocket']});
 
     this.socket.on('listaVideos', (videos: any[]) => {
       this.videos = videos.map((video, index) => {
@@ -28,11 +29,10 @@ export class PrincipalComponent  implements OnInit {
         return {key, value: video[key]};
       });
     });
-
   }
 
   generarCodi() {
-    this.socket = io('http://localhost:3080', {transports: ['websocket']});
+    this.socket = io('http://192.168.1.134:7777', {transports: ['websocket']});
 
     this.socket.on('codeFromServer', (data: any) => {
       console.log('Código del servidor:', data.code);
